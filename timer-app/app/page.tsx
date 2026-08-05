9ce4f53314334e850350cace9588f2ce43164e1f
torywash <washt@uni.edu>
2026-08-04 21:30:55 -0500

    started creation of root ui

--- timer-app/app/page.tsx
+++ timer-app/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
+
+  // set needed variables
+  const timer = '00:00:00'
+  const session_count = 'Session 0'
+
   return (
-    <div>
-      <h1>Hello, Study Timer!</h1>
+    <div className="container">
+      <h1>Study Timer</h1>
+      <div className="top-level">
+        <hr /> {/* TODO: Figure out how to make a divider to make a smooth UI */}
+        <h1>{timer}</h1>
+        <h2>{session_count}</h2>
+
+        <div className="controls">
+          <button>Start</button>
+          <button>Pause</button>
+          <button>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
